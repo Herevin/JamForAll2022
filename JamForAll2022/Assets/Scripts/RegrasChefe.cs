d0b7390693e46051132c4d90f330ae36862c440f
Vini Shoji <viniciusasato@gmail.com>
2022-11-05 20:34:25 -0300

    Consertado Bug de limite de index(array), e as regras se repetindo

--- JamForAll2022/Assets/Scripts/RegrasChefe.cs
+++ JamForAll2022/Assets/Scripts/RegrasChefe.cs
@@ -18,7 +18,7 @@ public class RegrasChefe : Passageiro
     //Acessorio          10
 
 
-    public bool[] avaliacoes = new bool[10];
+    [HideInInspector]public bool[] avaliacoes = new bool[11];
 
     [SerializeField] private TextMeshProUGUI[] regras;
 
@@ -28,8 +28,10 @@ public class RegrasChefe : Passageiro
     }
     public void RandomizarRegra(int quantidade)
     {
+        
         int totalRegras = quantidade;
         ResetarBooleanas();
+
         while (quantidade > 0)
         {
             
@@ -37,15 +39,23 @@ public class RegrasChefe : Passageiro
             switch (randomizador)
             {
                 case 0:
-                    if (avaliacoes[randomizador]) break;
+                    if (avaliacoes[randomizador]) 
+                        {
+                            quantidade += 1;
+                            break;
+                        }
                     avaliacoes[randomizador] = true;
                     idade = Random.Range(30, 61);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].gameObject.SetActive(true);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].text = idade < 45 ? "Menores que" + idade + " idade" : "Maiores que" + idade + " idade";
                     break;
                 case 1:
-                    if (avaliacoes[randomizador]) break;
-                   avaliacoes[randomizador] = true;
+                    if (avaliacoes[randomizador])
+                        {
+                            quantidade += 1;
+                            break;
+                        }
+                    avaliacoes[randomizador] = true;
                     altura = Random.Range(1.65f, 1.75f);
                     altura = Mathf.Round(altura * 100.0f) * 0.01f;
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].gameObject.SetActive(true);
@@ -53,14 +63,22 @@ public class RegrasChefe : Passageiro
 
                     break;
                 case 2:
-                    if (avaliacoes[randomizador]) break;
+                    if (avaliacoes[randomizador]) 
+                        {
+                            quantidade += 1;
+                            break;
+                        }
                     avaliacoes[randomizador] = true;
                     tipoPagamento = (TipoPagamento)Random.Range(0, 2);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].gameObject.SetActive(true);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].text = tipoPagamento.ToString();
                     break;
                 case 3:
-                    if (avaliacoes[randomizador]) break;
+                    if (avaliacoes[randomizador]) 
+                        {
+                            quantidade += 1;
+                            break;
+                        }
                     avaliacoes[randomizador] = true;
                     dinheiroParaPassagem += Random.Range(1, 3);
                     dinheiroParaPassagem -= Random.Range(1, 3);
@@ -68,49 +86,77 @@ public class RegrasChefe : Passageiro
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].text = dinheiroParaPassagem.ToString();
                     break;
                 case 4:
-                    if (avaliacoes[randomizador]) break;
+                    if (avaliacoes[randomizador]) 
+                        {
+                            quantidade += 1;
+                            break;
+                        }
                     avaliacoes[randomizador] = true;
                     corRoupa = (CorDaRoupa)Random.Range(0, 3);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].gameObject.SetActive(true);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].text = corRoupa.ToString();
                     break;
                 case 5:
-                    if (avaliacoes[randomizador]) break;
+                    if (avaliacoes[randomizador]) 
+                        {
+                            quantidade += 1;
+                            break;
+                        }
                     avaliacoes[randomizador] = true;
                     timeFutebol = (TimeFutebol)Random.Range(0, 3);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].gameObject.SetActive(true);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].text = timeFutebol.ToString();
                     break;
                 case 6:
-                    if (avaliacoes[randomizador]) break;
+                    if (avaliacoes[randomizador])
+                        {
+                            quantidade += 1;
+                            break;
+                        }
                     avaliacoes[randomizador] = true;
                     profissao = (Profissao)Random.Range(0, 3);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].gameObject.SetActive(true);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].text = profissao.ToString();
                     break;
                 case 7:
-                    if (avaliacoes[randomizador]) break;
+                    if (avaliacoes[randomizador]) 
+                        {
+                            quantidade += 1;
+                            break;
+                        }
                     avaliacoes[randomizador] = true;
                     geracaoPokemon = (GeracaoPokemon)Random.Range(0, 8);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].gameObject.SetActive(true);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].text = geracaoPokemon.ToString();
                     break;
                 case 8:
-                    if (avaliacoes[randomizador]) break;
+                    if (avaliacoes[randomizador]) 
+                        {
+                            quantidade += 1;
+                            break;
+                        }
                     avaliacoes[randomizador] = true;
                     roleLOL = (RoleLOL)Random.Range(0, 5);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].gameObject.SetActive(true);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].text = roleLOL.ToString();
                     break;
                 case 9:
-                    if (avaliacoes[randomizador]) break;
-                   avaliacoes[randomizador] = true;
+                    if (avaliacoes[randomizador])
+                    {
+                        quantidade += 1;
+                        break;
+                    }
+                    avaliacoes[randomizador] = true;
                     corCabelo = (CorDoCabelo)Random.Range(0, 3);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].gameObject.SetActive(true);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].text = corCabelo.ToString();
                     break;
                 case 10:
-                    if (avaliacoes[randomizador]) break;
+                    if (avaliacoes[randomizador])
+                    {
+                        quantidade += 1; 
+                        break;
+                    }
                     avaliacoes[randomizador] = true;
                     acessorios = (Acessorios)Random.Range(0, 3);
                     regras[Mathf.Abs(totalRegras - (quantidade - 1))].gameObject.SetActive(true);
@@ -140,8 +186,5 @@ public class RegrasChefe : Passageiro
         }
     }
 
-    private void AtualizarUI(int quantidade)
-    {
-        
-    }
+
 }
