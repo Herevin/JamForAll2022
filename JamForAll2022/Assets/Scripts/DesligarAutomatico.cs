e4d653f226c7277693881877420b016cea82d8b0
Vini Shoji <viniciusasato@gmail.com>
2022-11-06 21:27:42 -0300

    FixMensagens

--- JamForAll2022/Assets/Scripts/DesligarAutomatico.cs
+++ JamForAll2022/Assets/Scripts/DesligarAutomatico.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DesligarAutomatico : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
         StartCoroutine(Desativar());
     }
